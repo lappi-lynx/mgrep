848df1dda1034d782b50adcfdcb01e5f105a829e
extazystas <2153311+Extazystas@users.noreply.github.com>
2024-05-23 00:24:53 +0300

    Add basic grep functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 use std::env;
+use std::fs;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let path: &String = &args[2];
 
+    println!("Scanning for: {}", query);
+    println!("File path: {}", path);
+
+    let file_content = fs::read_to_string(path)
+        .expect("Should have been able to read the file");
+
+    for line in search(query, &file_content) {
+        println!("{line}");
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
